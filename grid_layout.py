c334b1e62ce874f85665f00b6de1e075b54fa480
woraseth <woraseth@users.noreply.github.com>
2015-09-16 20:26:13 +0700

    Update grid_layout.py

--- grid_layout.py
+++ grid_layout.py
@@ -3,17 +3,17 @@ from tkinter import *
 window = Tk()
 
 #--------------------------------------------
-label = Label(window, text='Name')
-label.grid(row=0, column=0, sticky=W)
+label0 = Label(window, text='Name')
+label0.grid(row=0, column=0, sticky=W)
 
-entry = Entry(window)
-entry.grid(row=0, column=1)
+entry0 = Entry(window)
+entry0.grid(row=0, column=1)
 #--------------------------------------------
-label = Label(window, text='Family Name')
-label.grid(row=1, column=0)
+label1 = Label(window, text='Family Name')
+label1.grid(row=1, column=0)
 
-entry = Entry(window)
-entry.grid(row=1, column=1)
+entry1 = Entry(window)
+entry1.grid(row=1, column=1)
 #--------------------------------------------
 button = Button(window, text="OK")
 button.grid(row=2, column=0, columnspan=2, sticky=W+E, padx=5, pady=5)
